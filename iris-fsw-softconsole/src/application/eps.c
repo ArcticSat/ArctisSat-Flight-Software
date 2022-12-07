324719cce80d8078672eb0a517f2dcfbae322d2b
jaydenmckoy <55809887+jaydenmckoy@users.noreply.github.com>
2022-12-06 23:47:34 -0600

    Added eps fram, adc, adcs commands IDs and some eps command handling.

--- iris-fsw-softconsole/src/application/eps.c
+++ iris-fsw-softconsole/src/application/eps.c
@@ -11,116 +11,104 @@
 void HandlePowerCommand(telemetryPacket_t * cmd_pkt)
 {
 	CANMessage_t cmd = {0};
+	cmd.id = POW_TXID;
 	switch(cmd_pkt->telem_id){
 		case POWER_READ_TEMP_CMD:{ // Read temperature value command
-			cmd.id = POW_TXID;
 			cmd.dlc = 2;
 			cmd.data[0] = POWER_READ_TEMP_CMD;
 			cmd.data[1] = cmd_pkt->data[0];
 			CAN_transmit_message(&cmd);
 			break;
 		}
-//		case POWER_READ_SOLAR_CURRENT_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_READ_SOLAR_CURRENT_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_READ_LOAD_CURRENT_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_READ_LOAD_CURRENT_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_READ_MSB_VOLTAGE_CMD:{ // Read solar current command
+		case POWER_READ_SOLAR_CURRENT_CMD:{ // Read solar current command
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_READ_SOLAR_CURRENT_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_READ_LOAD_CURRENT_CMD:{ // Read solar current command
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_READ_LOAD_CURRENT_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_READ_MSB_VOLTAGE_CMD:{ // Read solar current command
+			cmd.dlc = 1;
+			cmd.data[0] = POWER_READ_MSB_VOLTAGE_CMD;
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_GET_BATTERY_SOC_CMD:{ // Read solar current command
+			cmd.dlc = 1;
+			cmd.data[0] = POWER_GET_BATTERY_SOC_CMD;
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_GET_ECLIPSE_CMD:{ // Read solar current command
+			cmd.dlc = 1;
+			cmd.data[0] = POWER_GET_ECLIPSE_CMD;
+			CAN_transmit_message(&cmd);
+			break;
+		}
+//		case POWER_GET_BOOT_COUNT:{ // Read solar current command
+//			CANMessage_t cmd = {0};
 //			cmd.id = POW_TXID;
 //			cmd.dlc = 1;
-//			cmd.data[0] = POWER_READ_MSB_VOLTAGE_CMD;
+//			cmd.data[0] = POWER_GET_BOOT_COUNT_CMD;
 //			CAN_transmit_message(&cmd);
 //			break;
 //		}
-//		case POWER_GET_BATTERY_SOC_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 1;
-//			cmd.data[0] = POWER_GET_BATTERY_SOC_CMD;
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_GET_ECLIPSE_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 1;
-//			cmd.data[0] = POWER_GET_ECLIPSE_CMD;
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-////		case POWER_GET_BOOT_COUNT:{ // Read solar current command
-////			CANMessage_t cmd = {0};
-////			cmd.id = POW_TXID;
-////			cmd.dlc = 1;
-////			cmd.data[0] = POWER_GET_BOOT_COUNT_CMD;
-////			CAN_transmit_message(&cmd);
-////			break;
-////		}
-//		case POWER_SET_LOAD_OFF_CMD:{
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_SET_LOAD_OFF_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_SET_LOAD_ON_CMD:{
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_SET_LOAD_ON_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_SET_SOLAR_OFF_CMD:{
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_SET_SOLAR_OFF_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_SET_SOLAR_ON_CMD:{
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_SET_SOLAR_ON_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_SET_POW_MODE_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_SET_POW_MODE_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_AIT_SET_BATTERY_SOC_CMD:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 5;
-//			cmd.data[0] = POWER_AIT_SET_BATTERY_SOC_CMD;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
-//		case POWER_AIT_SET_ECLIPSE:{ // Read solar current command
-//			cmd.id = POW_TXID;
-//			cmd.dlc = 2;
-//			cmd.data[0] = POWER_AIT_SET_ECLIPSE;
-//			cmd.data[1] = cmd_pkt->data[0];
-//			CAN_transmit_message(&cmd);
-//			break;
-//		}
+		case POWER_SET_LOAD_OFF_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_SET_LOAD_OFF_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_SET_LOAD_ON_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_SET_LOAD_ON_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_SET_SOLAR_OFF_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_SET_SOLAR_OFF_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_SET_SOLAR_ON_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_SET_SOLAR_ON_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_SET_POW_MODE_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_SET_POW_MODE_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_AIT_SET_BATTERY_SOC_CMD:{
+			cmd.dlc = 5;
+			cmd.data[0] = POWER_AIT_SET_BATTERY_SOC_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
+		case POWER_AIT_SET_ECLIPSE_CMD:{
+			cmd.dlc = 2;
+			cmd.data[0] = POWER_AIT_SET_ECLIPSE_CMD;
+			cmd.data[1] = cmd_pkt->data[0];
+			CAN_transmit_message(&cmd);
+			break;
+		}
 		default:{
 			break;
 		}
