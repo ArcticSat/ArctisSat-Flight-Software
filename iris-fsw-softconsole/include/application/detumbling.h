55e28182331e769aec79528b667c7c2440aa13ad
jaydenmckoy <jpmckoy7@gmail.com>
2023-03-03 09:52:26 -0600

    Added source and header files for detumbling

---
+++ iris-fsw-softconsole/include/application/detumbling.h
@@ -0,0 +1,13 @@
+/*
+ * detumbling.h
+ *
+ *  Created on: Mar. 3, 2023
+ *      Author: Jayden McKoy
+ */
+
+#ifndef INCLUDE_APPLICATION_DETUMBLING_H_
+#define INCLUDE_APPLICATION_DETUMBLING_H_
+
+
+
+#endif /* INCLUDE_APPLICATION_DETUMBLING_H_ */
---
+++ iris-fsw-softconsole/src/application/detumbling.c
@@ -0,0 +1,8 @@
+/*
+ * detumbling.c
+ *
+ *  Created on: Mar. 3, 2023
+ *      Author: Jayden McKoy
+ */
+
+#include "application/detumbling.h"
