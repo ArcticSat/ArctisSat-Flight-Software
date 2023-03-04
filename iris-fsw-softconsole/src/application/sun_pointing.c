573b64db5c54d3227b76ee690480ce3cb08a8b18
Cody <codyaauch@gmail.com>
2023-03-04 11:01:22 -0600

    initial P1 setup

--- iris-fsw-softconsole/src/application/sun_pointing.c
+++ iris-fsw-softconsole/src/application/sun_pointing.c
@@ -6,13 +6,15 @@
  */
 
 #include "application/sun_pointing.h"
+#include <FreeRTOS-Kernel/include/FreeRTOS.h>
+#include "timers.h"
+#include "drivers/device/adcs_driver.h"
 
 #define SAMPLE_SUN_SENSOR_DUR 1
 #define SAMPLE_MAGS_CONTROL_DUR 4
 #define CYCLE_DUR 10
+#define SAMPLE_LOOPS 100
 
-#include <FreeRTOS-Kernel/include/FreeRTOS.h>
-#include "timers.h"
 
 // PROTOTYPES:
 void SunPointingP1( void );
@@ -60,7 +62,54 @@ void SunPointing( void )
     }
 }
 
-void SunPointingP1( void ){}
+void SunPointingP1( void )
+{
+    // Gyro Measurements & Conversion
+    /*
+        - Measurement (using adcs_driver.c function getGyroMeasurements) "returns" a 6-element array of uint8_ts (done)
+        - Create another function, which will convert the above array, into 3 floats in units of mdps
+            - Concatenate the high/low bytes for x,y,z (first into a uint16_t? or directly into a float) (a3g4250d_angular_rate_raw_get from ADCS code)
+            - Convert to mdps (a3g4250d_from_fs245dps_to_mdps from ADCS code)
+        - Result from this, should be a direct input into the control equation
+    */
+
+    // Sun Sensor Measurements & Conversion
+    /*
+        - Measurement (using adcs_driver.c function getSunSensorMeasurements) "returns" a 6-element array of uint8_ts (done)
+            - Add function to pull out solely pixel data
+        - What is the input to the control equation, and how to we convert from raw data??
+    */
+
+
+    // Main body
+    /*
+        - Turn the torque rods off
+        - Loop SAMPLING_LOOPS (100 for now) times
+        - In each loop:
+            - Do 1 gyro measurement and conversion
+            - Do 1 mag measurement and conversion
+            - Do 1 sun sensor measurement and conversion
+    */
+
+    // Turn off torque rods
+    setTorqueRodState(TORQUE_ROD_1, TR_STATE_OFF);
+    setTorqueRodState(TORQUE_ROD_2, TR_STATE_OFF);
+    setTorqueRodState(TORQUE_ROD_3, TR_STATE_OFF);
+
+    uint8_t mag_readings[SAMPLE_LOOPS];
+    uint8_t ss_readings[SAMPLE_LOOPS];
+
+    // Loop SAMPLE_LOOP times
+    for(int i=0; i<SAMPLE_LOOPS; i++)
+    {
+        /*
+            - Do 1 mag measurement and conversion
+            - Do 1 sun sensor measurement and conversion
+        */
+    }
+
+
+}
 
 void SunPointingP2( void ){}
 
